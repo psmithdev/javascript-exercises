876d3d8a41efc490321b76fe08b6dc9d09986d4f
parker <69760520+psmithdev@users.noreply.github.com>
2023-03-01 14:10:10 -0600

    reviewing functions and loops

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function () {
+  let string = "hey";
 
+  for (let num = 0; num < 3; num++) {
+    string += "" + num;
+  }
 };
+
+let x = 1;
+console.log(x);
 
 // Do not edit below this line
 module.exports = repeatString;
