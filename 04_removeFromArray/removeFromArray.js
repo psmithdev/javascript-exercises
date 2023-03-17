07523287e3a1f9836fa6ef00e95554762c7a15f0
parker <69760520+psmithdev@users.noreply.github.com>
2023-03-17 15:31:31 -0500

    practice passing arguments as array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, numbers) {
+  return array.slice(0, numbers).concat(array.slice(numbers + 1));
 };
 
 // Do not edit below this line
