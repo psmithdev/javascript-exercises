ee1710c53a86f2ba758bcbcd95d65a7ccdc71466
parker <69760520+psmithdev@users.noreply.github.com>
2023-04-18 12:01:02 -0500

    Temperature conversion with Math.round()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const result = ((fahrenheit - 32) * 5) / 9;
+  const rounded = Math.round(result * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const resultTwo = (celsius * 9) / 5 + 32;
+  const roundedTwo = Math.round(resultTwo * 10) / 10;
+  return roundedTwo;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
