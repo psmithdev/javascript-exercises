257910f588c7fc1c6732679489b4fc1e23020502
parker <69760520+psmithdev@users.noreply.github.com>
2023-04-18 09:16:05 -0500

    typeof == "number" worked

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function (integerOne, integerTwo) {
+  let result = 0;
 
+  if (
+    integerOne > integerTwo &&
+    integerOne > 0 &&
+    typeof integerTwo == "number"
+  ) {
+    for (let i = integerOne; i >= integerTwo; i--) {
+      result += i;
+    }
+    return result;
+  } else if (
+    integerOne < integerTwo &&
+    integerOne > 0 &&
+    typeof integerTwo == "number"
+  ) {
+    for (let i = integerOne; i <= integerTwo; i++) {
+      result += i;
+    }
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
+
+sumAll();
 
 // Do not edit below this line
 module.exports = sumAll;
